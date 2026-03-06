621be36c4469b844dc4106ad134088fdbf7193b2
lnsyzjp <lnsyzjp@163.com>
2026-03-06 22:46:16 +0800

    Site updated: 2026-03-06 22:46:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/06/android/","2026/03/01/hello-world/","2026/03/01/hello-world2/"];function toRandomPost(){
+var posts=["2026/03/01/hello-world2/","2026/03/06/android/","2026/03/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
